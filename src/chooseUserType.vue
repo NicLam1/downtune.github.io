5fc3649b5e501cf8ee45d13927ccd6880ec1c3d8
Dominic Chng <dominicchng.2023@scis.smu.edu.sg>
2024-10-18 16:27:34 +0800

    fuck
    fuckl

---
+++ src/chooseUserType.vue
@@ -0,0 +1,45 @@
+
+<template>
+    <body>
+	<div>
+		<img src="logo2-removebg-preview.png" alt="">
+	</div>
+	<div>
+		<nav class="menu">
+			<div class="menu__item">
+				<a class="menu__item-link">Artists</a>
+				<img class="menu__item-img" src="band1.jpg"/>
+				<img class="menu__item-img" src="band2.jpg"/>
+				<img class="menu__item-img" src="band3.jpg"/>
+				<img class="menu__item-img" src="band7.jpg"/>
+                <img class="menu__item-img" src="band5.jpg"/>
+				<div class="marquee">
+					<div class="marquee__inner">
+						<span>Artists</span>
+						<span>Artists</span>
+						<span>Artists</span>
+						<span>Artists</span>
+					</div>
+				</div>
+			</div>
+			<div class="menu__item">
+				<a class="menu__item-link">Listener</a>
+                <img class="menu__item-img" src="fan3.jpg"/>
+				<img class="menu__item-img" src="fan6.jpg"/>
+				<img class="menu__item-img" src="fan4.jpg"/>
+				<img class="menu__item-img" src="fan2.jpg"/>
+				<img class="menu__item-img" src="fan5.jpg"/>
+				<div class="marquee">
+					<div class="marquee__inner">
+						<span>Listener</span>
+						<span>Listener</span>
+						<span>Listener</span>
+						<span>Listener</span>
+					</div>
+				</div>
+			</div>
+		</nav>
+	</div>
+</body>
+<
+</template>
