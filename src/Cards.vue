88df4eab1b1972b585b1c4b136395bf2d50ac468
NicLam1 <nicholaslam.2023@scis.smu.edu.sg>
2024-10-19 00:51:19 +0800

    Update Cards.vue

--- src/Cards.vue
+++ src/Cards.vue
@@ -178,6 +178,7 @@ export default {
     handleLogin() {
       // Placeholder for login functionality
       console.log('Login button clicked');
+      window.location.href = '';
     }
   },
   mounted() {
