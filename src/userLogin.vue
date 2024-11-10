90d4f17d5e87d783363a78ec3f93c49ca7543955
Dominic Chng <dominicchng.2023@scis.smu.edu.sg>
2024-11-10 15:40:48 +0800

    test
    test

--- src/userLogin.vue
+++ src/userLogin.vue
@@ -1,190 +1,342 @@
 <template>
-  <div class="backgroundMain d-flex justify-content-center align-items-center w-100">
-    <div class="background" style="background-image: url('/img2/gig.jpg');"></div>
-    <div class="fog-container">
-      <div class="fog-img fog-img-first"></div>
-      <div class="fog-img fog-img-second"></div>
-    </div>
-    <div class="container min-h-screen d-flex justify-content-center align-items-center">
-      <div class="card w-50 h-100 shadow-lg overflow-hidden">
-        <div class="row g-0 h-100">
-          <!-- Login Form Section -->
-          <div class="col-md-8 col-12 p-5 col-flex d-flex flex-column">
-            <h2 class="text-center text-dark fw-bold mb-4">Sign in to your account</h2>
-            <form @submit.prevent="handleSubmit" class="d-flex flex-column justify-content-center">
-              <div class="mb-3">
-                <label for="email-address" class="form-label">Email address</label>
-                <input type="email" id="email-address" v-model="email" name="email" class="form-control" required />
-              </div>
-              <div class="mb-3">
-                <label for="password" class="form-label">Password</label>
-                <input type="password" id="password" v-model="password" name="password" class="form-control" required />
-              </div>
-              <div class="d-flex flex-column flex-md-row justify-content-between align-items-center mb-3">
-                <div class="form-check">
-                  <input class="form-check-input" type="checkbox" id="remember-me" v-model="rememberMe">
-                  <label class="form-check-label" for="remember-me">Remember me</label>
-                </div>
-                <a href="/forgot-password" class="text-primary mt-2 mt-md-0">Forgot your password?</a>
-              </div>
-              <button type="submit" class="btn btn-primary w-100">Sign in</button>
-            </form>
-            <p class="text-center mt-3">
-              Don't have an account?
-              <router-link to="/register/user" class="text-primary">
-                Sign up
-              </router-link>
-            </p>
-          </div>
 
-          <!-- Carousel Section -->
-          <!-- <div class="col-md-4 col-0 col-flex d-none d-md-block">
-            <div id="carouselExampleSlidesOnly" class="carousel slide h-100" data-bs-ride="carousel">
-              <div class="carousel-inner h-100">
-                <div class="carousel-item active">
-                  <img src="../images/stock band/image1.jpg" class="d-block w-100" alt="Slide 1">
-                </div>
-                <div class="carousel-item">
-                  <img src="../images/stock band/image2.jpg" class="d-block w-100" alt="Slide 2">
-                </div>
-                <div class="carousel-item">
-                  <img src="../images/stock band/image3.jpg" class="d-block w-100" alt="Slide 3">
-                </div>
-              </div>
+    <body>
+
+        <div id="app">
+            <div class="background" :class="{ 'fade-out': isFadingOut, 'fade-in': !isFadingOut }"
+                :style="{ backgroundImage: `url(${currentBackground})` }"></div> <!-- New div for background images -->
+
+            <div class="fog-container">
+                <div class="fog-img fog-img-first"></div>
+                <div class="fog-img fog-img-second"></div>
             </div>
-          </div> -->
+
+            <div class="login_wrap">
+                <div class="ring" id="hoverCircle" @mouseenter="changeBackground('/img2/gig2.jpg')"
+                    @mouseleave="changeBackground('/img2/gig.jpg')">
+                    <i></i>
+                    <i></i>
+                    <i></i>
+
+                    <div class="container">
+
+                        <h2 class="text-center text-dark fw-bold mb-4">Sign in to your listener account</h2>
+                        <form @submit.prevent="handleSubmit" class="d-flex flex-column justify-content-center">
+                            <div class="mb-3">
+                                <label for="email-address" class="form-label">Email address</label>
+                                <input type="email" id="email-address" v-model="email" name="email" class="form-control"
+                                    placeholder="Email address" required />
+                            </div>
+                            <div class="mb-3">
+                                <label for="password" class="form-label">Password</label>
+                                <input type="password" id="password" v-model="password" name="password"
+                                    class="form-control" placeholder="Password" required />
+                            </div>
+                            <div class="align-items-center mb-3 d-flex align-items-center justify-content-between">
+                                <div class="form-check d-flex flex-row-reverse align-items-center">
+                                    <input class="form-check-input" type="checkbox" id="remember-me"
+                                        v-model="rememberMe">
+                                    <label class="form-check-label me-2" for="remember-me">Remember me</label>
+                                </div>
+                                <a href="/forgot-password" class="text-primary">Forgot your password?</a>
+                            </div>
+                            <button type="submit" class="btn btn-primary w-100">Sign in</button>
+                        </form>
+
+                        <p class="text-center mt-3">
+                            Don't have an account?
+                            <router-link to="/register/user" class="text-primary">Sign up</router-link>
+                        </p>
+
+
+
+
+                    </div>
+
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  </div>
+
+
+    </body>
 </template>
 
 <script>
-import { auth } from '../firebaseConfig';
+
+import { auth } from '../firebaseConfig.js';
 import { signInWithEmailAndPassword } from 'firebase/auth';
 import { inject } from 'vue';
 
 export default {
-  data() {
-    return {
-      email: '',
-      password: '',
-      rememberMe: false,
-      error: null,
-      currentBackground: '/img2/gig.jpg' // Initial background image
-    };
-  },
-  setup() {
+    data() {
+        return {
+            currentBackground: '/img2/gig.jpg', // Initial background image
+            isFadingOut: false, // State for fading
+
+            email: '',
+            password: '',
+            rememberMe: false
+
+
+
+        };
+    },
+    setup() {
     const setLoginState = inject('setLoginState');
     return { setLoginState };
   },
-  methods: {
-    async handleSubmit() {
-      try {
-        const userCredential = await signInWithEmailAndPassword(auth, this.email, this.password);
-        const user = userCredential.user;
-        console.log("User successfully logged in:", user);
-        this.setLoginState(true, user.uid, user.displayName); // Store the user ID and display name
-        this.$router.push('/'); // Redirect to home page
-      } catch (error) {
-        console.error("Error logging in:", error);
-        this.error = error.message;
-      }
+
+    methods: {
+
+        // mouseevent
+
+        changeBackground(newImage) {
+            this.isFadingOut = true; // Start fading out
+
+            setTimeout(() => {
+                this.currentBackground = newImage; // Change the background image
+                this.isFadingOut = false; // End fading out
+            }, 1000);
+        },
+        async handleSubmit() {
+            try {
+                const userCredential = await signInWithEmailAndPassword(auth, this.email, this.password);
+                const user = userCredential.user;
+                console.log("User successfully logged in:", user);
+                this.setLoginState(true, user.uid, user.displayName); 
+                this.$router.push('/'); // Redirect to home page
+            } catch (error) {
+                console.error("Error logging in:", error);
+                this.error = error.message;
+            }
+        },
+
     }
-  }
-};
+
+
+}
+
+
+
+
 </script>
 
+
 <style scoped>
-body {
-  margin: 0;
-  padding: 0;
-  font-family: 'Poppins', sans-serif;
-  background: #000;
-  overflow: hidden;
+#app {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    position: relative;
+    width: 100%;
+    height: 100vh;
 }
 
-.background {
-  position: absolute;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-  background-size: cover;
-  background-position: center;
-}
 
 .fog-container {
-  position: absolute;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-  pointer-events: none;
-  overflow: hidden; /* Prevent horizontal scrolling */
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    overflow: hidden;
+    z-index: 2;
+    top: 0;
+    pointer-events: none;
 }
 
 .fog-img {
-  position: absolute;
-  height: 100%;
-  width: 200%; /* Adjust width to prevent horizontal scrolling */
-  z-index: 3;
+    position: absolute;
+    height: 100vh;
+    width: 300vw;
+    z-index: 3;
 }
 
+
 .fog-img-first {
-  background: url("/img2/fog-1.png");
-  background-repeat: repeat-x;
-  background-size: cover;
-  background-position: center;
-  animation: marquee 10s linear infinite; /* Adjusted duration */
+    background: url("/img2/fog-1.png");
+    background-repeat: repeat-x;
+    background-size: contain;
+    background-position: center;
+    animation: marquee 60s linear infinite;
+
 }
 
 .fog-img-second {
-  background: url("/img2/fog-2.png");
-  background-repeat: repeat-x;
-  background-size: cover;
-  background-position: center;
-  animation: marquee 60s linear infinite; /* Adjusted duration */
+    background: url("/img2/fog-2.png");
+    background-repeat: repeat-x;
+    background-size: contain;
+    background-position: center;
+    animation: marquee 30s linear infinite;
 }
 
 @keyframes marquee {
-  0% {
-    transform: translate3d(0, 0, 0);
-  }
-  100% {
-    transform: translate3d(-50%, 0, 0); /* Adjust to prevent horizontal scrolling */
-  }
+    0% {
+        transform: translate3d(0, 0, 0);
+    }
+
+    100% {
+        transform: translate3d(-200vw, 0, 0);
+    }
 }
 
-.min-h-screen {
-  min-height: 100vh;
+
+
+
+
+.container {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    width: 100%;
+    height: 82%;
+    z-index: 5;
 }
 
-.card {
-  border: none;
+
+
+body {
+    font-family: Arial, sans-serif;
+    margin: 0;
+    padding: 0;
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    overflow: hidden;
 }
 
-.col-flex {
-  flex: 1;
+.background {
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+
+    background-position: center center;
+    background-size: cover;
+    transition: opacity 1s ease-in-out;
+    z-index: 0;
+
 }
 
-.carousel-item {
-  height: 100%;
+.background.fade-out {
+    opacity: 0;
 }
 
-.carousel.slide {
-  height: 100%;
+.background.fade-in {
+    opacity: 1;
 }
 
-.carousel-item img {
-  width: 100%;
-  height: 100%;
-  object-fit: cover;
+
+
+
+* {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
 }
 
-.backgroundMain {
-  position: relative;
-  background: #000;
-  overflow: hidden;
+
+.login_wrap {
+    width: 500px;
+    margin: 0 auto;
+    padding: 150px 0;
+    position: relative;
+    z-index: 4;
+}
+
+.login_wrap .ring {
+    width: 550px;
+    height: 550px;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+}
+
+.login_wrap .ring>i {
+    border: 2px solid #ffffff;
+    transition: .5s;
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+}
+
+.login_wrap:hover .ring>i:nth-child(1) {
+    border: 6px solid #9ef9fc;
+    filter: drop-shadow(0 0 20px #9ef9fc);
+}
+
+.login_wrap .ring>i:nth-child(1) {
+    border-radius: 38% 62% 63% 37% / 41% 44% 56% 59%;
+    animation: animate1 6s linear infinite;
+}
+
+.login_wrap:hover .ring>i:nth-child(2) {
+    border: 6px solid #ff0057;
+    filter: drop-shadow(0 0 20px #ff0057);
+}
+
+.login_wrap .ring>i:nth-child(2) {
+    border-radius: 41% 44% 56% 59% / 38% 62% 63% 37%;
+    animation: animate1 6s linear infinite;
+}
+
+.login_wrap .ring>i:nth-child(3) {
+    border-radius: 41% 44% 56% / 38% 62% 63% 37%;
+    animation: animate2 6s linear infinite;
+}
+
+.login_wrap:hover .ring>i:nth-child(3) {
+    border: 6px solid #d081f7;
+    filter: drop-shadow(0 0 20px #d081f7);
+}
+
+
+
+
+
+@keyframes animate1 {
+    0% {
+        transform: rotate(0deg);
+    }
+
+    100% {
+        transform: rotate(360deg);
+    }
+}
+
+
+@keyframes animate2 {
+    0% {
+        transform: rotate(360deg);
+    }
+
+    100% {
+        transform: rotate(0deg);
+    }
+}
+
+
+.login_wrap {
+    position: relative;
+}
+
+
+
+
+.container {
+    padding: 10px 10px;
+    border-radius: 8px;
+    width: 300px;
+    text-align: center;
+    background-color: rgba(128, 128, 128, 0.5);
+    overflow: hidden;
 }
 </style>
--- src/userRegister.vue
+++ src/userRegister.vue
@@ -1,47 +1,55 @@
 <template>
-  <div v-if="!isRegistered" class="backgroundMain d-flex justify-content-center align-items-center w-100">
-    <!-- Registration Form Section (already exists) -->
-    <div class="background" style="background-image: url('/img2/gig.jpg');"></div>
-    <div class="fog-container">
-      <div class="fog-img fog-img-first"></div>
-      <div class="fog-img fog-img-second"></div>
-    </div>
-    <div class="container min-h-screen d-flex justify-content-center align-items-center">
-      <div class="card w-50 h-100 shadow-lg overflow-hidden">
-        <div class="row g-0 h-100">
-          <div class="col-md-8 col-12 p-5 col-flex d-flex flex-column">
-            <h2 class="text-center text-dark fw-bold mb-4">Register</h2>
-            <form @submit.prevent="handleRegister" class="d-flex flex-column justify-content-center">
-              <div class="mb-3">
-                <label for="email-address" class="form-label">Email address</label>
-                <input type="email" v-model="email" class="form-control" id="email" required />
-              </div>
-              <div class="mb-3">
-                <label for="username" class="form-label">Username</label>
-                <input type="text" v-model="username" class="form-control" id="username" required />
-              </div>
-              <div class="mb-3">
-                <label for="password" class="form-label">Password</label>
-                <input type="password" v-model="password" class="form-control" id="password" required />
-              </div>
-              <div class="mb-3">
-                <label for="password-confirm" class="form-label">Confirm Password</label>
-                <input type="password" v-model="confirmPassword" class="form-control" id="confirmPassword" required />
-              </div>
-              <p v-if="error" class="text-danger">{{ error }}</p>
-              <button type="submit" class="btn btn-primary w-100">Sign Up</button>
-            </form>
-            <p class="text-center mt-3">
-              Have an account?
-              <router-link to="/login/user" class="text-primary">
-                Sign In
-              </router-link>
-            </p>
-          </div>
+    <div v-if="!isRegistered" class="register">
+        <div class="background" :class="{'fade-out': isFadingOut, 'fade-in': !isFadingOut}" :style="{ backgroundImage: `url(${currentBackground})` }"></div>  <!-- New div for background images -->
+
+        <div class="fog-container">
+            <div class="fog-img fog-img-first"></div>
+            <div class="fog-img fog-img-second"></div>
         </div>
-      </div>
+
+        <div class="login_wrap">
+            <div class="ring" id="hoverCircle" @mouseenter="changeBackground('/img2/gig2.jpg')" @mouseleave="changeBackground('/img2/gig.jpg')">
+                <i></i>
+                <i></i>
+                <i></i>
+              
+                <div class="container">
+                    <div class="w-100">
+                        <h2 class="text-center text-dark fw-bold mb-4">Register listener</h2>
+                        <form @submit.prevent="handleRegister">
+                        <div class="mb-3">
+                            <label for="email-address" class="form-label">Email address</label>
+                            <input type="email" v-model="email" class="form-control" id="email" required />
+                        </div>
+                        <div class="mb-3">
+                            <label for="username" class="form-label">Username</label>
+                            <input type="text" v-model="username" class="form-control" id="username" required />
+                        </div>
+                        <div class="mb-3">
+                            <label for="password" class="form-label">Password</label>
+                            <input type="password" v-model="password" class="form-control" id="password" required />
+                        </div>
+                        <div class="mb-3">
+                            <label for="password-confirm" class="form-label">Confirm Password</label>
+                            <input type="password" v-model="confirmPassword" class="form-control" id="confirmPassword" required />
+                        </div>
+                        <p v-if="error" class="text-danger">{{ error }}</p>
+                        <button type="submit" class="btn btn-primary w-100">Sign Up</button>
+                        </form>
+                        <p class="text-center mt-3">
+                            Have an account?
+                            <router-link to="/login/user" class="text-primary">Sign In</router-link>
+                        </p>
+
+                    </div>
+                </div>
+
+               
+
+            </div>
+        </div>
     </div>
-  </div>
+
 
   <!-- Question Container Section (Visible after registration) -->
   <div v-else>
@@ -72,6 +80,9 @@ export default {
   },
   data() {
     return {
+    currentBackground: '/img2/gig.jpg', // Initial background image
+    isFadingOut: false, // State for fading
+    
       email: '',
       username: '',
       password: '',
@@ -84,45 +95,52 @@ export default {
   setup() {
     const setLoginState = inject('setLoginState');
     return { setLoginState };
-  },
-  methods: {
+},
+    
+
+
+    
+methods: {
+
+    // mouseevent
+
+    changeBackground(newImage) {
+        this.isFadingOut = true; // Start fading out
+        
+        setTimeout(() => {
+            this.currentBackground = newImage; // Change the background image
+            this.isFadingOut = false; // End fading out
+        }, 1000); 
+    },
+
+    
     async handleRegister() {
-      this.error = '';
+    this.error = '';
 
-
-      const user = auth.currentUser;
-      if (user) {
-        this.isRegistered = true; // Skip registration if user is logged in
-        this.user = user;  // Assign the current user object
-      }
-
-      // Basic validation for password matching
-      if (this.password !== this.confirmPassword) {
+    // Basic validation for password matching
+    if (this.password !== this.confirmPassword) {
         this.error = "Passwords do not match.";
         return;
-      }
+    }
 
-      try {
+    try {
         // Create the user in Firebase Authentication
         const userCredential = await createUserWithEmailAndPassword(auth, this.email, this.password);
         const user = userCredential.user;
 
         // Set the username as the display name in Firebase
         await updateProfile(user, {
-          displayName: this.username
+        displayName: this.username
         });
 
         console.log("User successfully registered:", user);
         this.isRegistered = true;
-        this.user = user;  // Store the user object
         this.setLoginState(true, user.uid, user.displayName); 
-      } catch (error) {
-        console.error("Error during registration:", error);
-        this.error = error.message;
-      }
+        } catch (error) {
+            console.error("Error during registration:", error);
+            this.error = error.message;
+        }
     },
-
-    // Method to update user preferences in Firestore after they answer the questions
     async updatePreferences(preferences) {
       try {
         const userId = this.user.uid;  // Use the current user's UID
@@ -134,101 +152,253 @@ export default {
       }
     }
   },
+        
+
+    // Method to update user preferences in Firestore after they answer the questions
+  
 };
 </script>
 
+
 <style scoped>
-body {
-  margin: 0;
-  padding: 0;
-  font-family: 'Poppins', sans-serif;
-  background: #000;
-  overflow: hidden;
+
+
+#app {
+    display: flex; 
+    justify-content: center; 
+    align-items: center; 
+    position: relative; 
+    width: 100%; 
+    height: 100vh;
 }
 
-.background {
-  position: absolute;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-  background-size: cover;
-  background-position: center;
-}
 
 .fog-container {
-  position: absolute;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-  pointer-events: none;
-  overflow: hidden; /* Prevent horizontal scrolling */
+    position: absolute; 
+    width: 100%;
+    height: 100%;
+    overflow: hidden;
+    z-index: 2; 
+    top: 0; 
+    pointer-events: none; 
 }
 
 .fog-img {
-  position: absolute;
-  height: 100%;
-  width: 200%; /* Adjust width to prevent horizontal scrolling */
-  z-index: 3;
+    position: absolute;
+    height: 100vh; 
+    width: 300vw; 
+    z-index: 3; 
 }
 
+
 .fog-img-first {
-  background: url("/img2/fog-1.png");
-  background-repeat: repeat-x;
-  background-size: cover;
-  background-position: center;
-  animation: marquee 10s linear infinite; /* Adjusted duration */
+background: url("/img2/fog-1.png");
+background-repeat: repeat-x;
+background-size: contain;
+background-position: center;
+animation: marquee 60s linear infinite;
+
 }
 
 .fog-img-second {
-  background: url("/img2/fog-2.png");
-  background-repeat: repeat-x;
-  background-size: cover;
-  background-position: center;
-  animation: marquee 60s linear infinite; /* Adjusted duration */
+background: url("/img2/fog-2.png");
+background-repeat: repeat-x;
+background-size: contain;
+background-position: center;
+animation: marquee 30s linear infinite;
 }
 
 @keyframes marquee {
-  0% {
+0% {
     transform: translate3d(0, 0, 0);
-  }
-  100% {
-    transform: translate3d(-50%, 0, 0); /* Adjust to prevent horizontal scrolling */
-  }
 }
 
-.min-h-screen {
-  min-height: 100vh;
+100% {
+    transform: translate3d(-200vw, 0, 0);
+}
 }
 
-.card {
-  border: none;
+
+
+
+
+.container {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    position: absolute; 
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%); 
+    width: 100%; 
+    height: 82%; 
+    z-index: 5; 
 }
 
-.col-flex {
-  flex: 1;
+
+
+.register {
+    margin: 0;
+    padding: 0;
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    overflow: hidden; 
 }
 
-.carousel-item {
-  height: 100%;
+.background {
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    
+    background-position: center center;
+    background-size: cover; 
+    transition: opacity 1s ease-in-out; 
+    z-index: 0; 
+    
 }
 
-.carousel.slide {
-  height: 100%;
+.background.fade-out {
+    opacity: 0; 
 }
 
-.carousel-item img {
-  width: 100%;
-  height: 100%;
-  object-fit: cover;
+.background.fade-in {
+    opacity: 1; 
 }
 
-.backgroundMain {
-  position: relative;
-  background: #000;
-  overflow: hidden;
+
+
+
+* {
+	margin: 0;
+	padding: 0;
+	box-sizing: border-box;
 }
+
+
+.login_wrap {
+	width: 500px;
+	margin: 0 auto;
+	padding: 150px 0;
+	position: relative;
+    z-index: 4; 
+}
+
+.login_wrap .ring {
+    width: 550px;
+    height: 550px;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+}
+
+.login_wrap .ring > i {
+	border: 2px solid #ffffff;
+	transition: .5s;
+	position: absolute;
+	top: 0;
+	left: 0;
+	width: 100%;
+	height: 100%;
+}
+
+.login_wrap:hover .ring > i:nth-child(1) {
+	border: 6px solid #9ef9fc;
+	filter: drop-shadow( 0 0 20px #9ef9fc);
+}
+
+.login_wrap .ring > i:nth-child(1) {
+	border-radius: 38% 62% 63% 37% / 41% 44% 56% 59%;
+	animation: animate1 6s linear infinite;
+}
+
+.login_wrap:hover .ring > i:nth-child(2) {
+	border: 6px solid #ff0057;
+	filter: drop-shadow( 0 0 20px #ff0057);
+}
+
+.login_wrap .ring > i:nth-child(2) {
+	border-radius: 41% 44% 56% 59% / 38% 62% 63% 37%;
+	animation: animate1 6s linear infinite;
+}
+
+.login_wrap .ring > i:nth-child(3) {
+	border-radius: 41% 44% 56% / 38% 62% 63% 37%;
+	animation: animate2 6s linear infinite;
+}
+
+.login_wrap:hover .ring > i:nth-child(3) {
+	border: 6px solid #d081f7;
+	filter: drop-shadow( 0 0 20px #d081f7);
+}
+
+
+
+
+
+@keyframes animate1 {
+	0% {
+		transform: rotate(0deg);
+	}
+	100% {
+		transform: rotate(360deg);
+	}
+}
+
+
+@keyframes animate2 {
+	0% {
+		transform: rotate(360deg);
+	}
+	100% {
+		transform: rotate(0deg);
+	}
+}
+
+
+.login_wrap {
+    position: relative; 
+}
+
+
+.playBut {
+    display: flex; 
+    justify-content: flex-end; 
+    align-items: center;       
+    position: absolute; 
+    top: 620px; 
+    right: 100px; 
+    z-index: 4;
+}
+
+.playBut p {
+    margin-right: 10px; 
+}
+
+img {
+    width: 80px;
+}
+
+#icon{
+    cursor: pointer;
+}
+
+
+.container {
+    padding: 10px 10px;
+    border-radius: 8px;
+    width: 300px;
+    text-align: center;
+    background-color: rgba(128, 128, 128, 0.5);
+    overflow: hidden; 
+}
+
+
+
+
 </style>
-
-<!-- Bootstrap -->
